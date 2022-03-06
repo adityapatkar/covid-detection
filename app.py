9a8b9877c78f736584c0e8d642c8210886670798
Aditya Patkar <aditya@Adityas-MacBook-Air.local>
2022-03-06 15:32:08 +0530

    Formatted code

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import streamlit as st 
+import streamlit as st
 import os
 import torch
 from torch.utils.data import Dataset, random_split, DataLoader
@@ -6,7 +6,8 @@ from PIL import Image
 import torchvision.models as models
 import matplotlib.pyplot as plt
 import torchvision.transforms as transforms
-#from sklearn.metrics import f1_score
+
+# from sklearn.metrics import f1_score
 import torch.nn.functional as F
 import torch.nn as nn
 from torchvision.utils import make_grid
@@ -16,14 +17,14 @@ from detect import *
 
 def main():
     # Render the readme as markdown using st.markdown.
-    page_bg_img = '''
+    page_bg_img = """
     <style>
     .stApp {
     background-image: url('https://unsplash.com/photos/ZiQkhI7417A');
     background-size: cover;
     }
     </style>
-    '''
+    """
     st.markdown(page_bg_img, unsafe_allow_html=True)
     hide_streamlit_style = """
     
@@ -32,16 +33,17 @@ def main():
     footer {visibility: hidden;}
     </style>
     """
-    st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
     # Once we have the dependencies, add a selector for the app mode on the sidebar.
     st.sidebar.title("What to do")
-    app_mode = st.sidebar.selectbox("Choose the app mode",
-        ["Show Instructions", "Diagnose Yourself"])
+    app_mode = st.sidebar.selectbox(
+        "Choose the app mode", ["Show Instructions", "Diagnose Yourself"]
+    )
     if app_mode == "Show Instructions":
-        st.sidebar.success('To continue select an option.')
+        st.sidebar.success("To continue select an option.")
         st.title("instructions")
         st.subheader("Welcome to Diagnose.ai! Here is how to use our app! :nerd_face: ")
-        
+
         st.write("Please Select Diagnose Yourself from the sidebar")
         st.markdown("""---""")
 
@@ -50,44 +52,47 @@ def main():
         st.write()
 
         if choice == "Browse an Image":
-            st.set_option('deprecation.showfileUploaderEncoding', False)
-            img= st.file_uploader("Upload Image", type=['jpg','png','jpeg'])
+            st.set_option("deprecation.showfileUploaderEncoding", False)
+            img = st.file_uploader("Upload Image", type=["jpg", "png", "jpeg"])
             if img is not None:
-                image = Image.open(img).convert('RGB')
+                image = Image.open(img).convert("RGB")
                 image = preprocess(image)
-                model = torch.load('./covidxray', map_location=torch.device('cpu'))
+                model = torch.load("./covidxray", map_location=torch.device("cpu"))
                 dataset = z()
                 img = image
                 col1, col2 = st.columns(2)
 
                 col1.subheader("Original Image")
-                fig = plt.figure(figsize = (15,15))
+                fig = plt.figure(figsize=(15, 15))
                 plt.imshow(img.permute(1, 2, 0).clamp(0, 1))
                 col1.pyplot(fig, use_column_width=True)
                 st.markdown("""---""")
                 st.text(f"Predicted by AI :  {predict_image(img, model)}")
                 st.markdown("""---""")
                 st.text("This is just for preliminary Diagnosis")
-                st.text("If you suffer from the symptoms, please see a doctor / radiologist.")
+                st.text(
+                    "If you suffer from the symptoms, please see a doctor / radiologist."
+                )
         else:
-            image = Image.open("./data/image.jpeg").convert('RGB')
+            image = Image.open("./data/image.jpeg").convert("RGB")
             image = preprocess(image)
-            model = torch.load('./covidxray', map_location=torch.device('cpu'))
+            model = torch.load("./covidxray", map_location=torch.device("cpu"))
             dataset = z()
             img = image
             col1, col2 = st.columns(2)
 
             col1.subheader("Your X-Ray")
-            fig = plt.figure(figsize = (15,15))
+            fig = plt.figure(figsize=(15, 15))
             plt.imshow(img.permute(1, 2, 0).clamp(0, 1))
             col1.pyplot(fig, use_column_width=True)
             st.markdown("""---""")
             st.text(f"Predicted by AI :  {predict_image(img, model)}")
             st.markdown("""---""")
             st.text("This is just for preliminary Diagnosis")
-            st.text("If you suffer from the symptoms, please see a doctor / radiologist.")
+            st.text(
+                "If you suffer from the symptoms, please see a doctor / radiologist."
+            )
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
